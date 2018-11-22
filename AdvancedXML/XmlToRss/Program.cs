8d0ab7ff3f6af8063ead7f6a889f803a366da9d6
Artsem Zhaldak <Artsem_Zhaldak@epam.com>
2018-11-22 14:21:45 +0300

    Task 2 has been completed

---
+++ AdvancedXML/XmlToRss/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Xsl;
+
+namespace XmlToRss
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+
+			if (!ValidateArgs(args))
+			{
+				Console.WriteLine("Something was wrong");
+				return;
+			}
+
+			try
+			{
+				PerformTransformation(args);
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.Message);
+				throw;
+			}
+
+			Console.WriteLine("The File has been successfully transformed.");
+		}
+
+		private static void PerformTransformation(string[] args)
+		{
+			var stylesheetPath = ConfigurationManager.AppSettings["XmlToRss_Xslt"];
+
+			try
+			{
+				var transform = new XslCompiledTransform();
+				transform.Load(stylesheetPath);
+
+				transform.Transform(args[0], "BookRss.xml");
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.Message);
+				throw ex;
+			}
+		}
+
+		private static bool ValidateArgs(string[] args)
+		{
+			if (args.Length != 1)
+			{
+				Console.WriteLine("Wrong number of parameters.(Enter one parameter)");
+
+				return false;
+			}
+			try
+			{
+				using (var file = File.Open(args[0], FileMode.Open))
+				{
+				}
+			}
+			catch (IOException ex)
+			{
+				Console.WriteLine(ex.Message);
+
+				return false;
+			}
+
+			return true;
+		}
+	}
+}
---
+++ AdvancedXML/XmlToRss/Xslt/XmlToRss.xslt
@@ -0,0 +1,54 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" version="1.0" xmlns:b="http://library.by/catalog">
+
+  <xsl:output method="xml" indent="yes"/>
+
+  <xsl:template match="/">
+	<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+	  <channel>
+		<title>Task 2. XML to RSS</title>
+		<link></link>
+		<description>Book catalog</description>
+		<xsl:apply-templates />
+	  </channel>
+	</rss>
+  </xsl:template>
+
+  <xsl:template match="b:book">
+	<item>
+	  <xsl:apply-templates select="b:title"/>
+	  <xsl:choose>
+		<xsl:when test="b:genre='Computer' and b:isbn">
+		  <link>
+			<xsl:value-of select="concat('http://my.safaribooksonline.com/', b:isbn)"/>
+		  </link>
+		</xsl:when>
+		<xsl:otherwise>
+		  <link></link>
+		</xsl:otherwise>
+	  </xsl:choose>
+	  <xsl:apply-templates select="b:description"/>
+	  <xsl:apply-templates select="b:publish_date"/>
+	</item>
+  </xsl:template>
+
+  <xsl:template match="b:title">
+	<title>
+	  <xsl:value-of select="." />
+	</title>
+  </xsl:template>
+  
+  <xsl:template match="b:description">
+	<description>
+	  <xsl:value-of select="." />
+	</description>
+  </xsl:template>
+
+  <xsl:template match="b:publish_date">
+	<pubDate>
+	  <xsl:value-of select="." />
+	</pubDate>
+  </xsl:template>
+  
+</xsl:stylesheet>
+<!--http://my.safaribooksonline.com/<isbn>/-->
